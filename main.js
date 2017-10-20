43057520ba5499f743251cd90a2160ebfb5ad06d
AdrianWille <31559389+AdrianWille@users.noreply.github.com>
2017-10-20 09:24:34 -0700

    I coded my 8 ball page until it could tell me my future

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
+var list = ["We shall see",
+			"Yes",
+			"No!",
+			"Mayve.",
+			"Ask again later.",
+			"As I see it...yes.",
+			"Certainly.",
+			"Try again later.",
+			"Doubtful.",
+			"Signs point to yes."];
 
+function randomNumber(){
+	return Math.floor(Math.random() * 10);	
+}
 
+			//just testing javascript
 function testJs(){
 	alert("test!");
 }
 
+//this function handles clicking of 8 ball image
 function shake8Ball(){
 	document.getElementById("magic8ball2").src="magic8ball2.jpg";
-	alert("We shall see!")
+	//alert("We shall see!")
+
+		//change paragraph to magic 8 ball text
+		document.getElementById("results").innerHTML=list[randomNumber()];
+
+		//alert(randomNumber())
 }
