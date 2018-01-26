e269b3028474c4688097ecf59fe6f720643c91a9
AdrianWille <31559389+AdrianWille@users.noreply.github.com>
2018-01-26 09:39:50 -0800

    I almost finished my flash card

---
+++ flashcards.js
@@ -0,0 +1,17 @@
+
+//adition operation
+function addNumbers(){
+
+		//get first and second number of textbook
+		var operand1 = document.getElementById("operand1").value;
+		var operand2 = document.getElementById("operand2").value;
+		
+		//add to second number of second textbook
+		var sum = parseInt(operand1) + parseInt(operand2);
+		
+		//test sum
+		alert(sum); 
+		
+		
+		//check if the sum equals the answer textbook
+} 
